60f524770d05e6ab4229e0ca5601d4606685d765
mohamed <abokirshm@gmail.com>
2025-08-13 22:51:05 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     // base: '/simple-react-ecommerce/',
   plugins: [react()],
-    base: '/FLAAKKO_/', // 
+    // base: '/FLAAKKO_/', // 
 });
